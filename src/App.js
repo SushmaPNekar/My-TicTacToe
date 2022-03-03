6ed4ae8519b3f550777bf35b0e2257cd8669699b
SushmaPNekar <87525377+SushmaPNekar@users.noreply.github.com>
2022-03-03 17:15:59 +0530

    created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ParentComp from './TicTacToe/ParentComp';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 >
+         Tic tac toe
+         </h1>
+      <ParentComp/>
+    </div>
+  );
+}
+
+export default App;
